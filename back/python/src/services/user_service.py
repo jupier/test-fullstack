03a113bc2c394c26a2b3dd2b089ff6612b34700a
jupier <jpier@etik.com>
2024-06-21 09:04:16 +0200

    keep only node backend

--- back/python/src/services/user_service.py
+++
@@ -1,7 +0,0 @@
-from db import fetch_one
-
-
-class UserService:
-    @staticmethod
-    def get_user(user_id):
-        return fetch_one("SELECT * FROM users WHERE id = %s" % user_id)
--- back/scala/README.md
+++
@@ -1,9 +0,0 @@
-# Scala Play! backend
-
-Welcome to the Scala world!
-
-To launch it:
-- `sbt run`
-
-App is running on debug mode on port `9000`.
-
