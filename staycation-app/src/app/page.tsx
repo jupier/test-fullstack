9a2bfaa0c6d91f5dd8eaf37007f41a776dd5533a
jupier <jpier@etik.com>
2024-06-22 12:24:47 +0200

    fetch hotel list

--- staycation-app/src/app/page.tsx
+++ staycation-app/src/app/page.tsx
@@ -1,11 +1,34 @@
-import { getUser } from "@/services/userService";
+import { getHotels, Hotel } from "@/services/hotelService";
+
+type HotelListProps = {
+  hotels: Hotel[];
+};
+
+const HotelList = ({ hotels }: HotelListProps) => {
+  return hotels.map((hotel) => {
+    return (
+      <ul>
+        <li>
+          {hotel.id} - {hotel.name} - {hotel.pictureId} - {hotel.preview} -{" "}
+          {hotel.stars}
+        </li>
+      </ul>
+    );
+  });
+};
 
 export default async function Home() {
-  const user = await getUser();
+  let hotels: Hotel[] = [];
+  let errorOccured: boolean = false;
+  try {
+    hotels = await getHotels();
+  } catch (e) {
+    errorOccured = true;
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello Next
-      <p>User first name: {user}</p>
+      {!errorOccured && <HotelList hotels={hotels} />}
+      {errorOccured && "An error occured when retrieving the hotel list :("}
     </main>
   );
 }
